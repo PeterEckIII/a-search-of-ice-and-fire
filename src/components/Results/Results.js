35b2f3005c70abdca5368939983591e732c9f3cc
Peter Eck <jpeckiii@gmail.com>
2019-09-20 18:41:00 -0500

    Finish initial Results component

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -3,15 +3,7 @@ import React from 'react'
 const Results = (props) => {
     return (
         <div>
-            <ul>
-                { props.data.map(result => {
-                    return (
-                        <li id={result.ObjectID}>
-                            { result }
-                        </li>
-                    )
-                }) }
-            </ul>
+            { props.data[ 0 ].map(result => <div>{ result }</div>) }
         </div>
     )
 }
